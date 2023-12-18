dc77d2272bc5203046ff791a99bc7f19feaee2ec
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2023-12-18 15:10:24 +0100

    inscription lycéen + validation

--- src/Entity/Lyceen.php
+++ src/Entity/Lyceen.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: LyceenRepository::class)]
 class Lyceen
@@ -17,6 +18,7 @@ class Lyceen
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
