16a1e56b7cf637d52264d1e22e2359e89fa5a0aa
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2023-12-18 17:55:53 +0100

    remove import

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\InscriptionRepository;
 use App\Repository\LyceenRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
