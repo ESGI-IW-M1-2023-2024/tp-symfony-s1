b5b6801004e5f53466c199b18c18e179d450096a
lorada.andre <lorada.andre@omerin.com>
2023-12-18 14:19:15 +0100

    crud - ressource +  salle + secteur + sponsor

---
+++ src/Form/RessourceType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Atelier;
+use App\Entity\Ressource;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RessourceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('type')
+            ->add('Contenu')
+            ->add('atelier', EntityType::class, [
+                'class' => Atelier::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ressource::class,
+        ]);
+    }
+}
