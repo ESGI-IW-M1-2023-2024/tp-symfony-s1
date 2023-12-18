030cac8a65664820f1a8f60d86ec7cb58fd0c44a
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2023-12-18 11:59:46 +0100

    feat: crud competences intervenant lycee lyceen

---
+++ src/Form/CompetenceType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Competence;
+use App\Entity\Metier;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompetenceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description')
+            ->add('metier', EntityType::class, [
+                'class' => Metier::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Competence::class,
+        ]);
+    }
+}
