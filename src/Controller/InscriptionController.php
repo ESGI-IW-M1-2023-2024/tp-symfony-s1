65034f03642d1306667639dabfe6c15798efdf4f
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2023-12-19 14:39:35 +0100

    page resume atelier

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 use App\Repository\LyceenRepository;
-use App\Repository\InscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/inscription', name: 'app_inscription')]
 class InscriptionController extends AbstractController
 {
-    #[Route('/inscription/byLycee/{idLycee}', name: 'app_inscription_par_lycee')]
+    #[Route('/byLycee/{idLycee}', name: 'app_inscription_par_lycee')]
     public function index($idLycee, LyceenRepository $lyceenRepository): Response
     {
         $lyceens = $lyceenRepository->findByLycee($idLycee);
@@ -22,5 +23,15 @@ class InscriptionController extends AbstractController
         return $this->render('inscription/index.html.twig', [
             'ateliers' => $ateliers,
         ]);
-    }    
+    }
+
+    #[Route('/resume', name: 'app_inscription_resume', methods: ['GET','POST'])]
+    public function resume(Request $request, LyceenRepository $lyceenRepository): Response
+    {
+        $id = $request->query->get('id');
+        $lyceen = $lyceenRepository->findOneBy(['id' => $id]);
+        return $this->render('inscription/resume.html.twig', [
+            'lyceen' => $lyceen,
+        ]);
+    }
 }
